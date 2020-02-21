21bafdecc3078825c9dba7d66364ee1dd311abba
Jamie Tartell <jetartell@gmail.com>
2020-02-21 03:22:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,3 +31,22 @@ function appendKitten() {
 function prependKitten() {
   return ["Arnold",...kittens]
 }
+
+function 
+  describe('removeLastKitten()', function() {
+    it('removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged', function() {
+      window.kittens = ['Milo', 'Otis', 'Garfield'];
+      expect(removeLastKitten()).toEqual(["Milo", "Otis"])
+
+      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
+    })
+  })
+
+  describe('removeFirstKitten()', function() {
+    it('removes the first kitten from the kittens array and returns a new array, leaving the kittens array unchanged', function() {
+      window.kittens = ['Milo', 'Otis', 'Garfield'];
+      expect(removeFirstKitten()).toEqual(["Otis", "Garfield"])
+
+      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
+    })
+  })
